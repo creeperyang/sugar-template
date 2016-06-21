5001cb7ed2613d246c30c332f1691243edbca488
creeperyang <yangcreeper@hotmail.com>
2016-06-21 12:52:03 +0800

    add some util function

--- src/utils.js
+++ src/utils.js
@@ -12,10 +12,28 @@ function typeStr(obj) {
     return isArray(obj) ? 'array' : typeof obj;
 }
 
+function isEmpty(value) {
+    if (!value) {
+        return value !== 0
+    } else if (isArray(value)) {
+        return value.length === 0
+    } else {
+        return false
+    }
+}
+
 const nonSpaceRe = /\S/
 
 function isWhitespace(string) {
     return !nonSpaceRe.test(string)
+}
+
+// values, not property name
+// "string", 'string', 5, false, true, null, undefined
+function isValue(string) {
+    return typeof string === 'string'
+        ? /true|false|null|undefined|([0-9\.]+)|"[^"]*"|'[^']*'/.test(string)
+        : false
 }
 
 function escapeRegExp(string) {
@@ -34,9 +52,11 @@ const entityMap = {
 }
 
 function escapeHtml(string) {
-    return String(string).replace(/[&<>"'`=\/]/g, function fromEntityMap(s) {
-        return entityMap[s]
-    })
+    return (string instanceof SafeString)
+        ? string.toString()
+        : String(string).replace(/[&<>"'`=\/]/g, function fromEntityMap(s) {
+            return entityMap[s]
+        })
 }
 
 /**
@@ -47,12 +67,75 @@ function hasProperty(obj, propName) {
     return obj != null && typeof obj === 'object' && (propName in obj)
 }
 
-module.exports = {
+const hashStringRe = /(\S+)=(true|false|null|undefined|([0-9\.]+)|"[^"]*"|'[^']*')/
+
+function parseParams(array) {
+    let context, hash = {}, val
+    // if has context
+    if (!hashStringRe.test(array[0])) {
+        context = array[0]
+        array = array.slice(1)
+    }
+    array.forEach((v) => {
+        val = hashStringRe.exec(v)
+        if (val) {
+            hash[val[1]] = getValueFromString(val[2], !!val[3])
+        }
+    })
+    return {
+        context,
+        hash
+    }
+}
+
+function parseFilters(array) {
+    return array.map(v => {
+        const val = parseParams(v.split(/\s+/))
+        return {
+            name: val.context,
+            hash: val.hash
+        }
+    })
+}
+
+const stringToValueMap = {
+    'null': null,
+    'undefined': undefined,
+    'true': true,
+    'false': false
+}
+
+function getValueFromString(string, preferNumber) {
+    if (string in stringToValueMap) {
+        return stringToValueMap[string]
+    }
+    return preferNumber ? Number(string) : string.slice(1, -1)
+}
+
+class SafeString {
+    constructor(raw) {
+        this.raw = raw
+    }
+    toString() {
+        return String(this.raw)
+    }
+    toJSON() {
+        return this.raw
+    }
+}
+
+exports = module.exports = {
     isFunction,
     isArray,
     typeStr,
     isWhitespace,
+    isEmpty,
+    isValue,
     escapeRegExp,
     escapeHtml,
-    hasProperty
+    hasProperty,
+    parseParams,
+    parseFilters,
+    getValueFromString,
+    SafeString
 }
