486c1d88130b310311217881963d041047358869
creeperyang <yangcreeper@hotmail.com>
2016-06-24 18:20:19 +0800

    export more API

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const sugar = require('./src/sugar')
 const Writer = require('./src/writer')
 const {
     typeStr,
-    escapeHtml
+    escapeHtml,
+    SafeString
 } = require('./src/utils')
 
 const defaultWriter = new Writer()
@@ -34,6 +35,26 @@ sugar.render = function render(template, view, partials) {
     return defaultWriter.render(template, view, partials)
 }
 
+sugar.registerHelper = function registerHelper(...args) {
+    return defaultWriter.registerHelper(...args)
+}
+sugar.unregisterHelper = function unregisterHelper(...args) {
+    return defaultWriter.unregisterHelper(...args)
+}
+sugar.registerFilter = function registerFilter(...args) {
+    return defaultWriter.registerFilter(...args)
+}
+sugar.unregisterFilter = function unregisterFilter(...args) {
+    return defaultWriter.unregisterFilter(...args)
+}
+sugar.registerPartial = function registerPartial(...args) {
+    return defaultWriter.registerPartial(...args)
+}
+sugar.unregisterPartial = function unregisterPartial(...args) {
+    return defaultWriter.unregisterPartial(...args)
+}
+
+sugar.SafeString = SafeString
 sugar.escape = escapeHtml
 
 module.exports = sugar
