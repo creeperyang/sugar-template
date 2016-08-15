cc1b08d56fe5e65965fbe3b6939db4ec70c6f30c
creeperyang <yangcreeper@hotmail.com>
2016-08-15 18:07:42 +0800

    export lib

---
+++ index.js
@@ -0,0 +1,20 @@
+const Sugar = require('./lib/sugar')
+const {
+    tokenizer,
+    parser,
+    traverser
+} = require('./lib/compiler')
+
+const instance = new Sugar()
+require('./lib/helpers/if')(instance)
+require('./lib/helpers/each')(instance)
+require('./lib/filters/string')(instance)
+
+exports = module.exports = instance
+exports.Sugar = Sugar
+exports.Compiler = {
+    tokenizer,
+    parser,
+    traverser
+}
+exports.Util = require('./lib/utils')
