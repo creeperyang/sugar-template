c50dc8b9f6a52f31e1a3008d10d3fd39e85667b3
creeperyang <yangcreeper@hotmail.com>
2017-01-24 12:52:13 +0800

    add test for Context

--- test/context.spec.js
+++ test/context.spec.js
@@ -18,6 +18,18 @@ describe('sugar-template#Context', function() {
         should(ctx.lookup('user.xyz')).be.exactly(undefined)
         should(ctx.lookup('user.xyz.abc')).be.exactly(undefined)
     })
+    it('`lookup` should get value correctly when specify dot!', function() {
+        const ctx2 = new Context(data, null, null, null)
+        should(ctx2.lookup('.')).be.exactly(null)
+        should(ctx2.lookup('this')).be.exactly(null)
+        ctx2.lookup('user.name').should.be.exactly('ABC')
+        ctx2.lookup('id').should.be.exactly(0)
+        should(ctx2.lookup('user.xyz')).be.exactly(undefined)
+        should(ctx2.lookup('user.xyz.abc')).be.exactly(undefined)
+        const ctx3 = new Context(data, null, null, data.user)
+        ctx3.lookup('.').should.be.exactly(data.user)
+        ctx3.lookup('this').should.be.exactly(data.user)
+    })
     it('`push` should gen sub context correctly!', function() {
         const subData = {
             id: 1
@@ -36,7 +48,7 @@ describe('sugar-template#Context', function() {
         const ctx2 = new Context(data, null, addtionalData)
         ctx2.lookup('.').should.deepEqual(data)
         ctx2.lookup('this').should.deepEqual(data)
-        ctx2.lookup('$$index').should.be.exactly(10)
+        ctx2.lookup('$index').should.be.exactly(10)
     })
     it('should not throw when data is null!', function() {
         const addtionalData = {
@@ -45,6 +57,6 @@ describe('sugar-template#Context', function() {
         const ctx2 = new Context(null, null, addtionalData)
         should(ctx2.lookup('.')).be.exactly(null)
         should(ctx2.lookup('this')).be.exactly(null)
-        ctx2.lookup('$$index').should.be.exactly(10)
+        ctx2.lookup('$index').should.be.exactly(10)
     })
 })
